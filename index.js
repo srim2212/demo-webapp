3c6c433556842b2ccfc3139b0fc66eeb232b1b1a
srim2212 <34409586+srim2212@users.noreply.github.com>
2024-08-08 18:19:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.port || 3000
+const port = process.env.port || 80
 
 app.get('/',(req,res)=>{
     res.send('Hi, this is demo web application!');
